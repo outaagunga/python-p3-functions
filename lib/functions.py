f9d02ce6aab5332c184bd3710ac73d11bbfe19e7
outaagunga <jeconiaagunga@gmail.com>
2023-08-16 23:53:31 +0300

    Completed the assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name= "Guido"):
+    print (f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print (f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return(45 + 55)
     pass
 
 def halve(number):
+    return (number/2.0)
     pass
+
